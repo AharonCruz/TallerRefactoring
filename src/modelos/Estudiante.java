cc21a219d3a6943fea16f17a7e5889820fa91523
Daniela <gdbaquer@espol.edu.ec>
2020-08-13 23:37:06 -0500

    Restructurando code smell codigo duplicado

--- src/modelos/Estudiante.java
+++ src/modelos/Estudiante.java
@@ -78,32 +78,19 @@ public class Estudiante{
         this.telefono = telefono;
     }
     
-    //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNotaInicial(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        double notaInicial=0;
+    //Calcula y devuelve la nota parcial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+    public double CalcularNotaParcial(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+        double notaParcial=0;
         for(Paralelo par:paralelos){
             if(p.equals(par)){
                 double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
                 double notaPractico=(ntalleres)*0.20;
-                notaInicial=notaTeorico+notaPractico;
+                notaParcial=notaTeorico+notaPractico;
             }
         }
-        return notaInicial;
+        return notaParcial;
     }
     
-    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    
-    public double CalcularNotaFinal(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
-        double notaFinal=0;
-        for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaFinal=notaTeorico+notaPractico;
-            }
-        }
-        return notaFinal;
-    }
     
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. Esta nota es solo el promedio de las dos calificaciones anteriores.
     public double CalcularNotaTotal(Paralelo p){
